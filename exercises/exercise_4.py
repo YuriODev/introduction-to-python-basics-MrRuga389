7a34bf695e63a2530236aee77dfb9b9651e39cd4
MrRuga389 <rugashane21@gmail.com>
2024-05-13 08:24:24 +0000

    solution

--- exercises/exercise_4.py
+++ exercises/exercise_4.py
@@ -1,2 +1,10 @@
-# Exercise 4
-# Your solution comes here
+number = int(input("please enter in a  digit number: "))
+
+num1 = number//1000
+num2 = (number//100)%10
+num3 = (number//10)%10
+num4 = (number%10)
+if num1 == num4 and num2 == num3:
+    print("1")
+else:
+    print("0")
