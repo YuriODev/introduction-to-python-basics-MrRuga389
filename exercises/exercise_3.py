fc9ecc033e99381f417662cb5f7b2590593d487b
MrRuga389 <rugashane21@gmail.com>
2024-05-13 08:18:31 +0000

    sol

--- exercises/exercise_3.py
+++ exercises/exercise_3.py
@@ -1,5 +1,5 @@
 seconds = int(input("please enter the amount of seconds that has passed: "))
 sec = seconds % 60
 minutes = (seconds//60)%60
-hour = (seconds//60)//60
-print(hour,":",minutes,":",sec)
+hour = (seconds//3600)%24
+print(f"{hour}:{minutes:02d}:{sec:02d}")
