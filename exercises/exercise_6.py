5774a20c83af4cbecc40826237afcee8c0f259c0
MrRuga389 <rugashane21@gmail.com>
2024-05-13 08:40:40 +0000

    sol

--- exercises/exercise_6.py
+++ exercises/exercise_6.py
@@ -1,2 +1,8 @@
-# Exercise 6
-# Your solution comes here
+x = int(input("input a number: "))
+y = int(input("input another number: "))
+
+condition1 = (x % y)>0
+condition2 = (x % y) == 0
+
+output = condition1 * "NO" or "YES"
+print(output)
