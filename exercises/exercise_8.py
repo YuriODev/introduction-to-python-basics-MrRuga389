b2ca8dc16f2d0a32a2a254cf197408436c575380
MrRuga389 <rugashane21@gmail.com>
2024-05-22 12:23:26 +0000

    SOLUTION

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -1,2 +1,11 @@
-# Exercise 8
-# Your solution comes here
+num1 = int(input("please input an integer: "))
+num2 = int(input("please input another integer: "))
+num3 = int(input("please input another integer: "))
+
+minimum = num1*(num1<=num2 and num1<=num3)+num2*(num2<num1 and num2<num3)+num3*(num3<num2 and num3<num1)
+max = num1*(num1>=num2 and num1>=num3)+num2*(num2>num1 and num2>num3)+num3*(num3>num2 and num3>num1)
+middle = (num1 + num2 + num3 - minimum - max)
+
+print(minimum)
+print(middle)
+print(max)
